02270791f866676e94d0136c492bda88556ba51f
AfteRRaIN-Sys <70005493+AfteRRaIN-Sys@users.noreply.github.com>
2021-06-01 12:06:16 +0700

    update stm32 and nodemcu code

--- nodeMCU/nodemcu.ino
+++ nodeMCU/nodemcu.ino
@@ -1,45 +1,47 @@
 /*  NETPIE ESP8266 basic sample                            */
 /*  More information visit : https://netpie.io             */
-#include <Arduino.h>
+
 #include <MicroGear.h>
 #include <ESP8266WiFi.h>
-//#include<ArduinoBearSSL.h>
 
 //ฝากใส่ username กับ pass wifi ด้วยนะ
-const char* ssid     = "a";
-const char* password = "00000000";
+const char* ssid     = <WIFI_SSID>;
+const char* password = <WIFI_KEY>;
 
 #define APPID   "SocialDistancingEscalator"
 #define KEY     "bBdIKogslWHhcGs"
 #define SECRET  "2cRbYPdx9Zt1yEy81Nmkxi67b"
 #define ALIAS   "NodeMCU"
-#define TargetWeb "HTML_web"
+#define TargrtWeb = "HTML_Web"
 
 WiFiClient client;
 
 MicroGear microgear(client);
 
-int8_t isFull = 0;
-int8_t out;
-
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
+//    Serial.print("Incoming message --> ");
+//    msg[msglen] = '\0';
+//    Serial.println((char *)msg);
+
+    Serial.print("Incoming message --> ");
+    Serial.println(topic);
+    Serial.println(" : ");
     char strState[msglen];
     for ( int i=0; i<msglen; i++) {
       strState[i] = (char)msg[i];
+      Serial.print((char)msg[i]);
     }
+    Serial.println();
 
-    String stateStr = String(strState).substring(0,msglen);
-    
+    String stateStr = String(strState).subString(0,msglen);
+
     if (stateStr == "YELLOW") {
-      Serial.println("Y");
-      delay(100);
-      isFull = 0;
+      digitalWrite(ledPin,HIGH) /* ไฟเหลืองเปิด ไม่รู้เขียนยังไง */
+      digitalWrite(ledPin,LOW) /* ไฟแดงปิด ไม่รู้เขียนยังไง */
+    } else if (stateStr == "RED") {
+      digitalWrite(ledPin,HIGH) /* ไฟแดงเปิด ไม่รู้เขียนยังไง */
+      digitalWrite(ledPin,LOW) /* ไฟเหลืองปิด ไม่รู้เขียนยังไง */
     }
-    else if (stateStr == "RED") {
-      Serial.println("R");
-      delay(50);
-      isFull = 1;
-   }
 }
 
 
@@ -85,72 +87,24 @@ void setup() {
 void loop() {
     /* To check if the microgear is still connected */
     if (microgear.connected()) {
-        //Serial.println("cnct");
+        Serial.println("connected");
 
         /* Call this method regularly otherwise the connection may be lost */
         microgear.loop();
 
         /* น่าจะต้องใส่โค้ดการอ่านค่าอะไรซักอย่างจาก senser */
+
+        char msg[128];
+        /* เอาค่า +1 หรือ -1 ใส่ msg */
+        /* ตัวอย่าง */
+//        String data = "+1";
+//        data.toCharArray(msg,data.length());
         
-        /*char test[2];
-        test[0] = '+';
-        test[1] = '1';
-        //microgear.publish("/test","1");
-        microgear.chat(TargetWeb,test);*/
-        
-        if (Serial.available()) {
-          //Serial.println("still connect");
-          int data_rcvd = Serial.read();   // read one byte from serial buffer and save to data_rcvd
-          //if (isFull==0 && data_rcvd == 1) {
-          if (data_rcvd == 1) {
-              //Serial.write(1);
-              delay(50);
-              //Serial.println("get 1");
-              delay(50);
-              // เอาค่า +1 หรือ -1 ใส่ msg
-              // ตัวอย่าง
-              char msg[10];
-              msg[0] = '+';
-              msg[1] = '1';  
-              microgear.chat(TargetWeb,msg);
-          }
-          else if (data_rcvd == 11) {
-              delay(50);
-              //Serial.println("get -1");
-              delay(50);
-              char msg[10];
-              msg[0] = '-';
-              msg[1] = '1';  
-              if (isFull == 1) isFull = 0;
-              microgear.chat(TargetWeb,msg);
-          }
-          //else {
-            //while (!Serial.available());
-            //if (isFull == 0) Serial.write(8);
-            if (isFull == 1){
-              out = 9;
-              Serial.write(9);
-            }
-          //}
-          /*else {
-            Serial.write(0);
-          }*/
-          
-        }
-        
+        microgear.chat(TargetWeb,msg);
     }
     else {
-        if (WiFi.status() == WL_CONNECTED) {
-          Serial.println("netpie connection lost");
-        }
-        else Serial.println("wifi connection lost, reconnect...");
+        Serial.println("connection lost, reconnect...");
         microgear.connect(APPID);
     }
-    //delay(200);
-    //delay(1000);
-    if (isFull == 1){
-      out = 9;
-      Serial.write(9);
-    }
-    delay(500);
+    delay(100);
 }
